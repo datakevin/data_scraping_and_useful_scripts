f5851ccdaa6c735d03f0c4c568de9a726da2083f
DataKevin <49355811+datakevin@users.noreply.github.com>
2019-04-22 23:56:38 -0700

    Update InstagramScraper.py

--- InstagramScraper.py
+++ InstagramScraper.py
@@ -1,3 +1,5 @@
+#the code is probably not great, made this when I was a Sophomore in College
+
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.action_chains import ActionChains
